513b37af5321f9c177fecb416d98d900ca37730b
Faurby <lasse_fk@hotmail.com>
2022-08-15 15:53:57 +0200

    Works with new template now

--- GroupGenerator.py
+++ GroupGenerator.py
@@ -1,30 +1,46 @@
 import csv
 import random
+import time
 from typing import final
 import pandas as pd
 
 # Read file xlsx
-excel_data = pd.read_excel('BSc-2021-nye-studerende-1.xlsx')
-
-# Convert to dataframe and specific fields
-studyLine = input("Study line (select between SWU, GBI, BDDIT, BDS): \n")
-df = pd.DataFrame(excel_data, columns=[studyLine])
-
-# Remove NaN values, convert to list and shuffle
-df.dropna(inplace=True)
+excel_data = pd.read_excel('Nye studerende 2022.xlsx')
+df = pd.DataFrame(excel_data)
 nestedLists = df.values.tolist()
-names = [x for xs in nestedLists for x in xs]
-random.shuffle(names)
 
+studyLineInput = input("Study line (select between SWU, GBI, DDIT, DS): \n")
+
+
+def getStudyLine(studyLineInput):
+    if studyLineInput.lower() == "swu":
+        return "Software Development"
+    elif studyLineInput.lower() == "gbi":
+        return "Global Business Informatics"
+    elif studyLineInput.lower() == "ddit":
+        return "Digital Design and Interactive Technologies"
+    else:
+        return "Data Science"
+
+
+# Create list with all students in study line
+students = [x for x in nestedLists if x[3] == getStudyLine(studyLineInput)]
+names = [x[0] for x in students]
+
+random.shuffle(names)
 peopleInGroup = int(input("How many people in a group? "))
 
 # Group generator function. Takes a list of names and a number of people in group.
-final_list = [names[i:i + peopleInGroup] for i in range(0, len(names), peopleInGroup)]
+final_list = [names[i:i + peopleInGroup]
+              for i in range(0, len(names), peopleInGroup)]
 
 # Print groups function
+
+
 def printGroups(list):
     for i in range(len(list)):
-        print("Group " + str(i+1) + ": " + str(list[i]))
+        print("Group " + str(i) + ": " + str(list[i]))
+
 
 # Check last group if it is not too small.
 last = final_list[-1]
@@ -33,14 +49,26 @@ if len(last) < peopleInGroup-1:
         final_list[-1].append(final_list[-2 - i][0])
         final_list[-2 - i] = final_list[-2 - i][1:]
 
+# Check if last group is only 1
+if len(final_list[-1]) == 1:
+    final_list[-2].append(final_list[-1][0])
+    final_list = final_list[:-1]
+
+print("Booting up...\n")
+time.sleep(1)
+print("Creating groups...\n")
+time.sleep(1)
+print("Groups created!\n")
+time.sleep(1)
+
 printGroups(final_list)
-print("\Successfully created groups, CSV file has been generated.\n")
+print("\nSuccessfully created groups, CSV file has been generated.\n")
 
 with open('miniprojektGrupper.csv', 'w', encoding="UTF8", newline='') as f:
     writer = csv.writer(f)
-    
+
     for i in range(len(final_list)):
-        writer.writerow(["Group " + str(i+1)])
-        for j in range (len(final_list[i])):
+        writer.writerow(["Group " + str(i)])
+        for j in range(len(final_list[i])):
             writer.writerow([final_list[i][j]])
         writer.writerow([])
---
+++ README.md
@@ -0,0 +1,14 @@
+# How to run this bad boi
+Have python3.10 installed
+
+Have pip3.10 installed
+
+To install dependencies if you need them:
+
+`pip install -r requirements.txt`
+
+To run:
+`python GroupGenerator.py`
+
+Input desired studyline and number of person in the group,
+as directed by the terminal.
